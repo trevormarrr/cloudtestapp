b9439b293207d1235ffb03253989af3d43280bc3
trevormarrr <tmarr2@my.gcu.edu>
2025-11-09 19:45:01 -0700

    ci: add GitHub Actions pipeline to Heroku pt2

--- CloudTestApp/obj/Debug/net8.0/CloudTestApp.AssemblyInfo.cs
+++ CloudTestApp/obj/Debug/net8.0/CloudTestApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CloudTestApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e85865c81d23f27e9e38328421d57774d461ccf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ff9572db0152b13da154de95253f8c20f653039")]
 [assembly: System.Reflection.AssemblyProductAttribute("CloudTestApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CloudTestApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
